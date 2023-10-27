7cd5fff8e184586f1121e76f3a65f01085f6c431
GuilhermeSouza <guilhermebencici@gmail.com>
2023-10-27 16:25:06 -0300

    Conceitos de Propriedade e Encapsulamento

---
+++ SOLIDCsharp/Aula_Propriedades/Pessoa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Propriedades
+{
+    internal class Pessoa
+    {   // CAMPO
+        private string nome;
+        private string genero;
+        private int idade;
+
+        // CONSTRUTOR PARA TERMOS ACESSO A PROPRIEDADE ( ENCAPSULAMENTO )
+        public Pessoa(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        //PROPRIEDADE NOME
+        public string Nome { get; }
+
+        //PROPRIEDADE GENERO
+        public string Genero
+        {
+            set
+            {
+                genero = value;
+                Console.WriteLine(genero);
+            }
+        }
+
+        //PROPRIEDADE IDADE
+        public int Idade
+        {
+            set
+            {
+                if(value < 18)
+                {
+                    Console.WriteLine("Idade precisa ser maior que 18");
+                }
+                else
+                {
+                    this.idade = value;
+                }
+            }
+            get { return idade; }
+        }
+
+
+
+    }
+
+
+}
---
+++ SOLIDCsharp/Aula_Propriedades/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Propriedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Guilherme");
+            p1.Genero = "Masculino";
+            p1.Idade = 19;
+
+            Console.WriteLine(p1.Nome);
+            Console.WriteLine(p1.Idade);
+        }
+    }
+}
