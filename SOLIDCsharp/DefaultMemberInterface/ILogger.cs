34bc526903b3618e59420e7ef5679a40e203d47f
GuilhermeSouza <guilhermebencici@gmail.com>
2023-11-02 22:43:07 -0300

    Default Member Interfaces

---
+++ SOLIDCsharp/DefaultMemberInterface/ILogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultMemberInterface
+{
+    internal interface ILogger
+    {
+        void Info(string message);
+
+        // NOVO MÉTODO .NET 8, PODE-SE TER INTERFACE COM MÉTODOS COM SUAS IMPLEMENTAÇÕES
+        void Error(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+    }
+}
